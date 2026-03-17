49864cb28967dff1c52cf7091bc39182d77e724b
João Henrique Lima <jhntreme13@gmail.com>
2026-03-17 16:08:44 -0300

    remoçao de pastas temporarias

--- packages/pitang-frontend/src/routes/dashboard/todos/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/dashboard/todos/")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Todos page</div>;
-}
